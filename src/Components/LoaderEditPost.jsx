41ba8bce43e9813edeb62fe8df5f2d77a70aa726
@adflipr <clouds744@gmail.com>
2025-09-12 01:34:43 +0530

    minor fix LoaderEditPost

--- src/Components/LoaderEditPost.jsx
+++ src/Components/LoaderEditPost.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Loader() {
   return (
-    <div className="flex justify-center items-center h-full w-full">
+    <div className="flex justify-center items-center h-full w-full min-h-150">
       <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
     </div>
   );
